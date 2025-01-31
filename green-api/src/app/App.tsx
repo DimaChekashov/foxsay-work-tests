dbdb759b4f7d8737cd2b9b83fd247f96efc9f120
DimaChekashov <foxdima99@gmail.com>
2025-01-31 11:51:24 +0300

    prepared the component architecture

---
+++ green-api/src/app/App.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto pt-20">
+        asdasdas
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ green-api/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="p-4 py-6 bg-lime-600 fixed top-0 left-0 right-0">
+        <h1 className="text-2xl font-bold text-white">Green API - Test Messager</h1>
+    </div>
+  );
+}
+
+export default Header;
