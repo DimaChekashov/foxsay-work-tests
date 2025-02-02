c23c708c792b47ff60b0ef347bdc5e627943dd0d
DimaChekashov <foxdima99@gmail.com>
2025-02-02 15:25:57 +0300

    save auth data into local storage

--- green-api/src/store/authStore.ts
+++ green-api/src/store/authStore.ts
@@ -8,12 +8,30 @@ interface AuthStore {
     logout: () => void;
 }
 
-const useAuthStore = create<AuthStore>((set) => ({
-    idInstance: "",
-    apiTokenInstance: "",
-    isLoggedIn: false,
-    login: (idInstance: string, apiTokenInstance: string) => set({ idInstance, apiTokenInstance, isLoggedIn: true }),
-    logout: () => set({ idInstance: "", apiTokenInstance: "", isLoggedIn: false }),
-}));
+const useAuthStore = create<AuthStore>((set) => {
+    const storedIdInstance = localStorage.getItem("idInstance") || "";
+    const storedApiTokenInstance = localStorage.getItem("apiTokenInstance") || "";
+    const storedIsLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+    
+    return {
+        idInstance: storedIdInstance,
+        apiTokenInstance: storedApiTokenInstance,
+        isLoggedIn: storedIsLoggedIn,
+        login: (idInstance: string, apiTokenInstance: string) => {
+            localStorage.setItem("idInstance", idInstance);
+            localStorage.setItem("apiTokenInstance", apiTokenInstance);
+            localStorage.setItem("isLoggedIn", "true");
+        
+            set({ idInstance, apiTokenInstance, isLoggedIn: true });
+        },
+        logout: () => {
+            localStorage.removeItem("idInstance");
+            localStorage.removeItem("apiTokenInstance");
+            localStorage.removeItem("isLoggedIn");
+
+            set({ idInstance: "", apiTokenInstance: "", isLoggedIn: false });
+        },
+    }
+});
 
 export default useAuthStore;
