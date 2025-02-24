d7dc34119193c4c4fea81df708062f6d982afff3
DimaChekashov <foxdima99@gmail.com>
2025-02-24 09:52:52 +0300

    fill html structure

--- cheelee/src/js/main.js
+++ cheelee/src/js/main.js
@@ -1,3 +1,28 @@
-const a = 1;
+function customSelect() {
+    const select = document.querySelector(".custom-select");
+    const trigger = select.querySelector(".select-trigger");
+    const options = select.querySelectorAll(".option");
+    const hiddenInput = select.querySelector("#selected-value");
+    
+    trigger.addEventListener("click", () => {
+        select.classList.toggle("active");
+    });
+    
+    options.forEach(option => {
+        option.addEventListener("click", () => {
+        trigger.textContent = option.textContent;
+        hiddenInput.value = option.dataset.value;
+        select.classList.remove("active");
+        });
+    });
+    
+    document.addEventListener("click", (e) => {
+        if (!select.contains(e.target)) {
+        select.classList.remove("active");
+        }
+    });
+}
 
-console.log(a);
+document.addEventListener("DOMContentLoaded", () => {
+    customSelect();
+});
