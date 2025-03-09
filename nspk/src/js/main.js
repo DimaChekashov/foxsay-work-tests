ca3e97d8e5819df414cb9a518581580f86e55d70
DimaChekashov <foxdima99@gmail.com>
2025-03-09 17:42:57 +0300

    complete cases

--- nspk/src/js/main.js
+++ nspk/src/js/main.js
@@ -1,5 +1,19 @@
-// const a = 1;
-
-// a = 2;
-
-console.log("Hello World!");
+document.addEventListener("DOMContentLoaded", () => {
+	$('#cases-slider').owlCarousel({
+    loop: false,
+    margin: 20,
+    nav: false,
+		dots: false,
+    responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			1200:{
+				items:3
+			}
+    }
+	})
+});
